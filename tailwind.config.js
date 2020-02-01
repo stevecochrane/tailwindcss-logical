37d61f40884bca205839baf8d883caa2dda2293c
Steve Cochrane <steve@stevecochrane.com>
2020-02-01 12:29:56 -0600

    Use implicit returns

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,37 +4,37 @@ module.exports = {
 		function({ addUtilities, theme }) {
 			const spacing = Object.entries(theme('spacing'));
 
-			const marginBlockStartUtilities = spacing.map(([key, value]) => {
-				return {
+			const marginBlockStartUtilities = spacing.map(([key, value]) => (
+				{
 					[`.mbs-${key}`]: {
 						marginBlockStart: value
 					}
-				};
-			});
+				}
+			));
 
-			const marginBlockEndUtilities = spacing.map(([key, value]) => {
-				return {
+			const marginBlockEndUtilities = spacing.map(([key, value]) => (
+				{
 					[`.mbe-${key}`]: {
 						marginBlockEnd: value
 					}
-				};
-			});
+				}
+			));
 
-			const marginInlineStartUtilities = spacing.map(([key, value]) => {
-				return {
+			const marginInlineStartUtilities = spacing.map(([key, value]) => (
+				{
 					[`.mis-${key}`]: {
 						marginInlineStart: value
 					}
-				};
-			});
+				}
+			));
 
-			const marginInlineEndUtilities = spacing.map(([key, value]) => {
-				return {
+			const marginInlineEndUtilities = spacing.map(([key, value]) => (
+				{
 					[`.mie-${key}`]: {
 						marginInlineEnd: value
 					}
-				};
-			});
+				}
+			));
 
 			addUtilities(marginBlockStartUtilities);
 			addUtilities(marginBlockEndUtilities);
