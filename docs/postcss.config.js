8025874da65edd113e2bf67bae45e6b0baba76f2
Steve Cochrane <steve@stevecochrane.com>
2020-02-18 21:37:09 -0600

    Rename demo/ to docs/ to work with GitHub Pages

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: [
-    require('tailwindcss')({ config: './demo/tailwind.config.js' }),
+    require('tailwindcss')({ config: './docs/tailwind.config.js' }),
     require('postcss-logical'),
     require('postcss-dir-pseudo-class')
   ]
