d906b900ab97b1173dd909dcd607cec98a40771b
Steve Cochrane <steve@stevecochrane.com>
2020-11-03 20:43:38 -0600

    Add test for non-configurable utilities

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,19 +2,67 @@ const postcss = require('postcss');
 const tailwindcss = require('tailwindcss');
 const plugin = require('../index.js');
 
-const generatePluginCss = () => {
-  return postcss(tailwindcss({
-      corePlugins: false,
-      plugins: [ plugin ]
-    }))
+const generatePluginCss = (options = {}) => {
+  return postcss(tailwindcss(options))
     .process('@tailwind utilities;', {
       from: undefined,
     })
     .then(result => result.css);
 };
 
-test('it outputs an empty string', () => {
-  return generatePluginCss({ variants: [] }).then(css => {
-    expect(css).toMatchCss(``);
-  });
+test('float, clear, text-align and resize', () => {
+  return generatePluginCss({
+      corePlugins: false,
+      theme: {
+        borderRadius: {},
+        borderWidth: {},
+        height: {},
+        inset: {},
+        margin: {},
+        maxHeight: {},
+        maxWidth: {},
+        minHeight: {},
+        minWidth: {},
+        padding: {},
+        spacing: {},
+        width: {}
+      },
+      plugins: [ plugin ],
+      variants: []
+    })
+    .then(css => {
+      expect(css).toMatchCss(`
+        .float-start {
+          float: inline-start
+        }
+
+        .float-end {
+          float: inline-end
+        }
+
+        .clear-start {
+          clear: inline-start
+        }
+
+        .clear-end {
+          clear: inline-end
+        }
+
+        .text-start {
+          text-align: start
+        }
+
+        .text-end {
+          text-align: end
+        }
+
+        .resize-block {
+          resize: block
+        }
+
+        .resize-inline {
+          resize: inline
+        }
+      `);
+    });
 });
