714c4bced526c39b807a530cb67a22415a02c6f7
Steve Cochrane <steve@stevecochrane.com>
2020-11-03 22:28:11 -0600

    Add basic test for block-size

--- tests/index.test.js
+++ tests/index.test.js
@@ -10,6 +10,40 @@ const generatePluginCss = (options = {}) => {
     .then(result => result.css);
 };
 
+const nonconfigurableStyles = `
+  .float-start {
+    float: inline-start
+  }
+
+  .float-end {
+    float: inline-end
+  }
+
+  .clear-start {
+    clear: inline-start
+  }
+
+  .clear-end {
+    clear: inline-end
+  }
+
+  .text-start {
+    text-align: start
+  }
+
+  .text-end {
+    text-align: end
+  }
+
+  .resize-block {
+    resize: block
+  }
+
+  .resize-inline {
+    resize: inline
+  }
+`;
+
 test('float, clear, text-align and resize', () => {
   return generatePluginCss({
       corePlugins: false,
@@ -31,37 +65,118 @@ test('float, clear, text-align and resize', () => {
       variants: []
     })
     .then(css => {
+      expect(css).toMatchCss(nonconfigurableStyles);
+    });
+});
+
+test('block-size, with default height and spacing configs', () => {
+  return generatePluginCss({
+      corePlugins: false,
+      theme: {
+        borderRadius: {},
+        borderWidth: {},
+        inset: {},
+        margin: {},
+        maxHeight: {},
+        maxWidth: {},
+        minHeight: {},
+        minWidth: {},
+        padding: {},
+        width: {}
+      },
+      plugins: [ plugin ],
+      variants: []
+    })
+    .then(css => {
       expect(css).toMatchCss(`
-        .float-start {
-          float: inline-start
+        ${nonconfigurableStyles}
+
+        .bs-0 {
+          block-size: 0
         }
 
-        .float-end {
-          float: inline-end
+        .bs-1 {
+          block-size: 0.25rem
         }
 
-        .clear-start {
-          clear: inline-start
+        .bs-2 {
+          block-size: 0.5rem
         }
 
-        .clear-end {
-          clear: inline-end
+        .bs-3 {
+          block-size: 0.75rem
         }
 
-        .text-start {
-          text-align: start
+        .bs-4 {
+          block-size: 1rem
         }
 
-        .text-end {
-          text-align: end
+        .bs-5 {
+          block-size: 1.25rem
         }
 
-        .resize-block {
-          resize: block
+        .bs-6 {
+          block-size: 1.5rem
         }
 
-        .resize-inline {
-          resize: inline
+        .bs-8 {
+          block-size: 2rem
+        }
+
+        .bs-10 {
+          block-size: 2.5rem
+        }
+
+        .bs-12 {
+          block-size: 3rem
+        }
+
+        .bs-16 {
+          block-size: 4rem
+        }
+
+        .bs-20 {
+          block-size: 5rem
+        }
+
+        .bs-24 {
+          block-size: 6rem
+        }
+
+        .bs-32 {
+          block-size: 8rem
+        }
+
+        .bs-40 {
+          block-size: 10rem
+        }
+
+        .bs-48 {
+          block-size: 12rem
+        }
+
+        .bs-56 {
+          block-size: 14rem
+        }
+
+        .bs-64 {
+          block-size: 16rem
+        }
+
+        .bs-auto {
+          block-size: auto
+        }
+
+        .bs-px {
+          block-size: 1px
+        }
+
+        .bs-full {
+          block-size: 100%
+        }
+
+        .bs-screen {
+          block-size: 100vh
         }
       `);
     });
