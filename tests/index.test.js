5584f341c28447f2e67e63c362cd92c3229d4bb0
Steve Cochrane <steve@stevecochrane.com>
2020-11-04 20:32:48 -0600

    Move nonconfigurable and block-size CSS output into separate modules

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,6 +2,11 @@ const postcss = require('postcss');
 const tailwindcss = require('tailwindcss');
 const plugin = require('../index.js');
 
+const nonconfigurableStyles = require('./output/nonconfigurable');
+const blockSizeStyles = require('./output/blockSize');
+const minBlockSizeStyles = require('./output/minBlockSize');
+const maxBlockSizeStyles = require('./output/maxBlockSize');
+
 const generatePluginCss = (options = {}) => {
   return postcss(tailwindcss(options))
     .process('@tailwind utilities;', {
@@ -9,40 +14,6 @@ const generatePluginCss = (options = {}) => {
     })
     .then(result => result.css);
 };
-
-const nonconfigurableStyles = `
-  .float-start {
-    float: inline-start
-  }
-
-  .float-end {
-    float: inline-end
-  }
-
-  .clear-start {
-    clear: inline-start
-  }
-
-  .clear-end {
-    clear: inline-end
-  }
-
-  .text-start {
-    text-align: start
-  }
-
-  .text-end {
-    text-align: end
-  }
-
-  .resize-block {
-    resize: block
-  }
-
-  .resize-inline {
-    resize: inline
-  }
-`;
 
 test('float, clear, text-align and resize', () => {
   return generatePluginCss({
@@ -90,94 +61,7 @@ test('block-size, with default height and spacing configs', () => {
     .then(css => {
       expect(css).toMatchCss(`
         ${nonconfigurableStyles}
-
-        .bs-0 {
-          block-size: 0
-        }
-
-        .bs-1 {
-          block-size: 0.25rem
-        }
-
-        .bs-2 {
-          block-size: 0.5rem
-        }
-
-        .bs-3 {
-          block-size: 0.75rem
-        }
-
-        .bs-4 {
-          block-size: 1rem
-        }
-
-        .bs-5 {
-          block-size: 1.25rem
-        }
-
-        .bs-6 {
-          block-size: 1.5rem
-        }
-
-        .bs-8 {
-          block-size: 2rem
-        }
-
-        .bs-10 {
-          block-size: 2.5rem
-        }
-
-        .bs-12 {
-          block-size: 3rem
-        }
-
-        .bs-16 {
-          block-size: 4rem
-        }
-
-        .bs-20 {
-          block-size: 5rem
-        }
-
-        .bs-24 {
-          block-size: 6rem
-        }
-
-        .bs-32 {
-          block-size: 8rem
-        }
-
-        .bs-40 {
-          block-size: 10rem
-        }
-
-        .bs-48 {
-          block-size: 12rem
-        }
-
-        .bs-56 {
-          block-size: 14rem
-        }
-
-        .bs-64 {
-          block-size: 16rem
-        }
-
-        .bs-auto {
-          block-size: auto
-        }
-
-        .bs-px {
-          block-size: 1px
-        }
-
-        .bs-full {
-          block-size: 100%
-        }
-
-        .bs-screen {
-          block-size: 100vh
-        }
+        ${blockSizeStyles}
       `);
     });
 });
@@ -204,18 +88,7 @@ test('min-block-size, with default minHeight config', () => {
     .then(css => {
       expect(css).toMatchCss(`
         ${nonconfigurableStyles}
-
-        .min-bs-0 {
-          min-block-size: 0
-        }
-
-        .min-bs-full {
-          min-block-size: 100%
-        }
-
-        .min-bs-screen {
-          min-block-size: 100vh
-        }
+        ${minBlockSizeStyles}
       `);
     });
 });
@@ -242,14 +115,7 @@ test('max-block-size, with default maxHeight config', () => {
     .then(css => {
       expect(css).toMatchCss(`
         ${nonconfigurableStyles}
-
-        .max-bs-full {
-          max-block-size: 100%
-        }
-
-        .max-bs-screen {
-          max-block-size: 100vh
-        }
+        ${maxBlockSizeStyles}
       `);
     });
 });
---
+++ tests/output/maxBlockSize.js
@@ -0,0 +1,9 @@
+module.exports = `
+  .max-bs-full {
+    max-block-size: 100%
+  }
+
+  .max-bs-screen {
+    max-block-size: 100vh
+  }
+`;
