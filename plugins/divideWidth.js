341fd155d5fa53a80c396d25b77b776170f8452f
Steve Cochrane <steve@stevecochrane.com>
2021-12-13 21:55:42 -0600

    Remove value logic in spaceBetween and divideWidth that isn't used

--- plugins/divideWidth.js
+++ plugins/divideWidth.js
@@ -1,26 +1,24 @@
 module.exports = function({ addUtilities, matchUtilities, theme }) {
   matchUtilities(
     {
-      'divide-b': (value) => {
-        value = value === '0' ? '0px' : value;
-        return {
+      'divide-b': (value) => (
+        {
           '& > :not([hidden]) ~ :not([hidden])': {
             '--tw-divide-b-reverse': '0',
             'border-block-start-width': `calc(${value} * calc(1 - var(--tw-divide-b-reverse)))`,
             'border-block-end-width': `calc(${value} * var(--tw-divide-b-reverse))`
           }
-        };
-      },
-      'divide-i': (value) => {
-        value = value === '0' ? '0px' : value;
-        return {
+        }
+      ),
+      'divide-i': (value) => (
+        {
           '& > :not([hidden]) ~ :not([hidden])': {
             '--tw-divide-i-reverse': '0',
             'border-inline-start-width': `calc(${value} * calc(1 - var(--tw-divide-i-reverse)))`,
             'border-inline-end-width': `calc(${value} * var(--tw-divide-i-reverse))`
           }
-        };
-      }
+        }
+      )
     },
     { values: theme('divideWidth') }
   );
