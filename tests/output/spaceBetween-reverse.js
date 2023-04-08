dead009c066a9b1aba383626d7059744f7001315
Steve Cochrane <steve@stevecochrane.com>
2023-04-07 21:26:08 -0500

    Update other nonconfigurable test output files

--- tests/output/spaceBetween-reverse.js
+++ tests/output/spaceBetween-reverse.js
@@ -2,7 +2,6 @@ module.exports = `
   .space-b-reverse > :not([hidden]) ~ :not([hidden]) {
     --tw-space-b-reverse: 1
   }
-
   .space-i-reverse > :not([hidden]) ~ :not([hidden]) {
     --tw-space-i-reverse: 1
   }
