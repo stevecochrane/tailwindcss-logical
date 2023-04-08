ba2963783e398281f75bbc66cf9e0aa026e9490b
Steve Cochrane <steve@stevecochrane.com>
2023-04-07 21:26:40 -0500

    Update blockSize test output

--- tests/output/blockSize.js
+++ tests/output/blockSize.js
@@ -2,212 +2,159 @@ module.exports = `
   .bs-0 {
     block-size: 0px
   }
-
-  .bs-1 {
-    block-size: 0.25rem
-  }
-
-  .bs-2 {
-    block-size: 0.5rem
-  }
-
-  .bs-3 {
-    block-size: 0.75rem
-  }
-
-  .bs-4 {
-    block-size: 1rem
-  }
-
-  .bs-5 {
-    block-size: 1.25rem
-  }
-
-  .bs-6 {
-    block-size: 1.5rem
-  }
-
-  .bs-7 {
-    block-size: 1.75rem
-  }
-
-  .bs-8 {
-    block-size: 2rem
-  }
-
-  .bs-9 {
-    block-size: 2.25rem
-  }
-
-  .bs-10 {
-    block-size: 2.5rem
-  }
-
-  .bs-11 {
-    block-size: 2.75rem
-  }
-
-  .bs-12 {
-    block-size: 3rem
-  }
-
-  .bs-14 {
-    block-size: 3.5rem
-  }
-
-  .bs-16 {
-    block-size: 4rem
-  }
-
-  .bs-20 {
-    block-size: 5rem
-  }
-
-  .bs-24 {
-    block-size: 6rem
-  }
-
-  .bs-28 {
-    block-size: 7rem
-  }
-
-  .bs-32 {
-    block-size: 8rem
-  }
-
-  .bs-36 {
-    block-size: 9rem
-  }
-
-  .bs-40 {
-    block-size: 10rem
-  }
-
-  .bs-44 {
-    block-size: 11rem
-  }
-
-  .bs-48 {
-    block-size: 12rem
-  }
-
-  .bs-52 {
-    block-size: 13rem
-  }
-
-  .bs-56 {
-    block-size: 14rem
-  }
-
-  .bs-60 {
-    block-size: 15rem
-  }
-
-  .bs-64 {
-    block-size: 16rem
-  }
-
-  .bs-72 {
-    block-size: 18rem
-  }
-
-  .bs-80 {
-    block-size: 20rem
-  }
-
-  .bs-96 {
-    block-size: 24rem
-  }
-
-  .bs-auto {
-    block-size: auto
-  }
-
-  .bs-px {
-    block-size: 1px
-  }
-
   .bs-0\\.5 {
     block-size: 0.125rem
   }
-
+  .bs-1 {
+    block-size: 0.25rem
+  }
   .bs-1\\.5 {
     block-size: 0.375rem
   }
-
-  .bs-2\\.5 {
-    block-size: 0.625rem
-  }
-
-  .bs-3\\.5 {
-    block-size: 0.875rem
-  }
-
   .bs-1\\/2 {
     block-size: 50%
   }
-
   .bs-1\\/3 {
     block-size: 33.333333%
   }
-
-  .bs-2\\/3 {
-    block-size: 66.666667%
-  }
-
   .bs-1\\/4 {
     block-size: 25%
   }
-
-  .bs-2\\/4 {
-    block-size: 50%
-  }
-
-  .bs-3\\/4 {
-    block-size: 75%
-  }
-
   .bs-1\\/5 {
     block-size: 20%
   }
-
-  .bs-2\\/5 {
-    block-size: 40%
-  }
-
-  .bs-3\\/5 {
-    block-size: 60%
-  }
-
-  .bs-4\\/5 {
-    block-size: 80%
-  }
-
   .bs-1\\/6 {
     block-size: 16.666667%
   }
-
+  .bs-10 {
+    block-size: 2.5rem
+  }
+  .bs-11 {
+    block-size: 2.75rem
+  }
+  .bs-12 {
+    block-size: 3rem
+  }
+  .bs-14 {
+    block-size: 3.5rem
+  }
+  .bs-16 {
+    block-size: 4rem
+  }
+  .bs-2 {
+    block-size: 0.5rem
+  }
+  .bs-2\\.5 {
+    block-size: 0.625rem
+  }
+  .bs-2\\/3 {
+    block-size: 66.666667%
+  }
+  .bs-2\\/4 {
+    block-size: 50%
+  }
+  .bs-2\\/5 {
+    block-size: 40%
+  }
   .bs-2\\/6 {
     block-size: 33.333333%
   }
-
+  .bs-20 {
+    block-size: 5rem
+  }
+  .bs-24 {
+    block-size: 6rem
+  }
+  .bs-28 {
+    block-size: 7rem
+  }
+  .bs-3 {
+    block-size: 0.75rem
+  }
+  .bs-3\\.5 {
+    block-size: 0.875rem
+  }
+  .bs-3\\/4 {
+    block-size: 75%
+  }
+  .bs-3\\/5 {
+    block-size: 60%
+  }
   .bs-3\\/6 {
     block-size: 50%
   }
-
+  .bs-32 {
+    block-size: 8rem
+  }
+  .bs-36 {
+    block-size: 9rem
+  }
+  .bs-4 {
+    block-size: 1rem
+  }
+  .bs-4\\/5 {
+    block-size: 80%
+  }
   .bs-4\\/6 {
     block-size: 66.666667%
   }
-
+  .bs-40 {
+    block-size: 10rem
+  }
+  .bs-44 {
+    block-size: 11rem
+  }
+  .bs-48 {
+    block-size: 12rem
+  }
+  .bs-5 {
+    block-size: 1.25rem
+  }
   .bs-5\\/6 {
     block-size: 83.333333%
   }
-
+  .bs-52 {
+    block-size: 13rem
+  }
+  .bs-56 {
+    block-size: 14rem
+  }
+  .bs-6 {
+    block-size: 1.5rem
+  }
+  .bs-60 {
+    block-size: 15rem
+  }
+  .bs-64 {
+    block-size: 16rem
+  }
+  .bs-7 {
+    block-size: 1.75rem
+  }
+  .bs-72 {
+    block-size: 18rem
+  }
+  .bs-8 {
+    block-size: 2rem
+  }
+  .bs-80 {
+    block-size: 20rem
+  }
+  .bs-9 {
+    block-size: 2.25rem
+  }
+  .bs-96 {
+    block-size: 24rem
+  }
+  .bs-auto {
+    block-size: auto
+  }
   .bs-full {
     block-size: 100%
   }
-
+  .bs-px {
+    block-size: 1px
+  }
   .bs-screen {
     block-size: 100vh
-  }
-`;
+  }`;
